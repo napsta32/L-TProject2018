87a0fe03acf0e01a72c40cfb2bafeab91dc0700a
Franco Peña <fpenac@pucp.pe>
2018-05-13 23:06:58 +0200

    Renamed too similar classes

--- src/main/scala/d3v4/Queue.scala
+++ src/main/scala/d3v4/Queue.scala
@@ -3,20 +3,20 @@ package d3v4
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
-@JSImport("d3-request", JSImport.Namespace)
+@JSImport("d3-queue", JSImport.Namespace)
 @js.native
 object d3queue extends js.Object {
-  def queue(): D3Queue = js.native
+  def queue(): RequestQueue = js.native
 }
 
 @js.native
-trait D3Queue extends js.Object {
+trait RequestQueue extends js.Object {
   type CallbackType = (String, js.Function) => Unit
 
   def defer(callback: CallbackType, args: Primitive*): this.type = js.native
   // def defer(callback: (String, js.Function1[js.Object, Unit]) => Unit, args: Primitive*): this.type = js.native
   // def defer(callback: (String, (js.Any, js.Array[js.Dictionary[String]]) => Unit) => Unit, args: Primitive*): this.type = js.native
   def abort(): this.type = js.native;
-  def await(callback: js.Function2[js.Any, js.Object, Unit]): this.type = js.native
+  // def await(callback: js.Function2[js.Any, js.Object, Unit]): this.type = js.native
   def await(callback: js.Function3[js.Any, js.Object, js.Object, Unit]): this.type = js.native
 }
